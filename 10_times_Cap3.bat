8fd0213354006817756ff6875345cd5924264f10
Enzo <vieira.enzof@gmail.com>
2023-05-09 19:52:10 -0300

    .

--- 10_times_Cap3.bat
+++ 10_times_Cap3.bat
@@ -3,12 +3,12 @@
 echo iniciando
 
 for /l %%i in (0,1,9) do (
-  echo Rodando 3-1:  %%i
+  echo -------------------------------- Rodando 3-1:  %%i --------------------------------
   Capitulo3\GoingFaster3-1_8000.exe
 )
 
 for /l %%i in (0,1,9) do (
-  echo Rodando 3-2:  %%i
+  echo -------------------------------- Rodando 3-2:  %%i --------------------------------
   Capitulo3\GoingFaster3-2_8000.exe
 )
 
