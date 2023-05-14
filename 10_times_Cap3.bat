6e25ac02d29ffbbab3515b64b8e7d7b1ea656cfb
Enzo <vieira.enzof@gmail.com>
2023-05-14 18:18:31 -0300

    feat: new file to run 4,5,6

--- 10_times_Cap3.bat
+++ 10_times_Cap3.bat
@@ -2,14 +2,19 @@
 
 echo iniciando
 
-for /l %%i in (0,1,2) do (
-  echo -------------------------------- Rodando 3-1:  %%i --------------------------------
-  Capitulo3\GoingFaster3-1_8000.exe
-)
-
 for /l %%i in (0,1,9) do (
-  echo -------------------------------- Rodando 3-2:  %%i --------------------------------
-  Capitulo3\GoingFaster3-2_8000.exe
+  echo -------------------------------- Rodando 4:  %%i --------------------------------
+  Capitulo4\GoingFaster4-OO_8000.exe
+  echo -------------------------------- Rodando 4-O3:  %%i --------------------------------
+  Capitulo4\GoingFaster4-O3_8000.exe
+  echo -------------------------------- Rodando 5:  %%i --------------------------------
+  Capitulo5\GoingFaster5-OO_8000.exe
+  echo -------------------------------- Rodando 5-O3:  %%i --------------------------------
+  Capitulo5\GoingFaster5-O3_8000.exe
+  echo -------------------------------- Rodando 6:  %%i --------------------------------
+  Capitulo6\GoingFaster6-OO_8000.exe
+  echo -------------------------------- Rodando 6-O3:  %%i --------------------------------
+  Capitulo6\GoingFaster6-O3_8000.exe
 )
 
 echo acabou;
